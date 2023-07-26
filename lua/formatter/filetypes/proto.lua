4294fdfe6e581c5dd3b462c6f3f02c961682624c
Mike Leone <mleone896@gmail.com>
2023-07-26 08:19:18 -0400

    {WIP}

--- lua/formatter/filetypes/proto.lua
+++ lua/formatter/filetypes/proto.lua
@@ -1,10 +1,9 @@
 local M = {}
 
 function M.buffmt()
-  local util = require("formatter.util")
   return {
     exe = "buf",
-    args = { "format", util.escape_path(util.get_current_buffer_file_name()), "-" },
+    args = { "format" },
     stdin = true,
   }
 end
